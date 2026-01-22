6a45a9fd0b492d304d34a77b56c39fa9ded65396
teawd <52975098+TeaWhyDee@users.noreply.github.com>
2026-01-22 23:39:05 +0100

    refactor & small fixes
    * split manual/auto modes into separate functions
    * fixed -f and -y options
    * added exceptions for better flow control
    * catching more exceptions
    * updated help and readme
    * generalized print_tag.py script.

--- README.md
+++ README.md
@@ -1,22 +1,60 @@
 Tag music files using Genius API (or manually).
-Supports most audio formats (using [music-tag](https://github.com/KristoforMaynard/music-tag) library).  
-Thanks to [wrap-genius](https://github.com/fedecalendino/wrap-genius) for genius API code.
+Supports most audio formats (using [music-tag](https://github.com/KristoforMaynard/music-tag) library).
+Uses [wrap-genius](https://github.com/fedecalendino/wrap-genius) for genius API code.
 
-This script attempts ro remove genius artifacting (links etc.)
+This script attempts ro remove Genius artifacting (links etc.)
 that sometimes appear in API responces.
+
+You will need a Genius API key to use the search functionality:
+https://docs.genius.com/
 
 
 # Requirements
-Dependencies in requirements.txt.
+Install dependencies in requirements.txt.
+
 Tested with Python 3.11.5, 3.15.
 
 # Run
+The script will run in an interactive mode by default.
+The script will always skip a file if the lyrics tag is already set,
+unless the force option is specified.
+
+Run `python main.py -h` for options.
 ```bash
 usage: main.py [-h] [-t TOKEN] [-s SEARCH_STRING] [-l LYRICS] [-m] [-f] [-y]
                files [files ...]
+               
+positional arguments:
+  files                 path(s) to file(s)
+
+options:
+  -h, --help            show this help message and exit
+  -t, --token TOKEN     Genius API token
+  -s, --search_string SEARCH_STRING
+                        set the Genius API search string (usually 'artist track_title')
+  -l, --lyrics LYRICS   set the provided lyrics non-interactively
+  -m, --manual          manual interactive mode. Manually set lyrics to one or multiple files
+  -f, --force           set the lyrics tag even the tag is already present
+  -y, --always_yes      set the lyrics tag without asking the user for confirmation
 ```
 
-# Develop
+Example usage `python main.py -t $TOKEN ./file.mp3 -s "Kkaydes For What"`:
+```
+./file.mp3
+--- Search string: Kkaydes For What
+--- Found song "Kkaydes - For What" with lyrics:
+For what?
+
+--- Are these lyrics correct? [<ctrl-d>=yes, input=correct lyrics]
+These are manually typed lyrics.
+--- Saving provided lyrics
+```
+
+There is an extra script `print_tag.py` for printing all tags or a specified tags.
+
+# Development
+This codebase uses exceptions for flow control, yikes!
+
 Create virtual environment (optional):
 ```bash
 python -m venv venv
--- main.py
+++ main.py
@@ -8,25 +8,54 @@ import music_tag
 from lyricsgenius import Genius
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Tagger")
+class NoGeniusSongFound(Exception):
+    def __init__(self, message, search_string):
+        super().__init__(message)
+        self.search_string = search_string
 
-    parser.add_argument("files", nargs="+", help="Path(s) to file(s)")
+
+class LyricsTagPresent(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="\
+Audio file lyrics tagger. \
+This script will extract the artist and track names, \
+look them up on Genius and set the 'lyrics' metadata tag.\
+"
+    )
+
+    parser.add_argument("files", nargs="+", help="path(s) to file(s)")
     parser.add_argument("-t", "--token", type=str, help="Genius token")
-    parser.add_argument("-s", "--search_string", type=str, help="Search string")
-    parser.add_argument("-l", "--lyrics", type=str, help="Lyrics to put")
-    parser.add_argument("-m", "--manual", action="store_true", help="Tag manally")
+    parser.add_argument(
+        "-s",
+        "--search_string",
+        type=str,
+        help="specify the Genius API search string (usually 'artist track_title')",
+    )
+    parser.add_argument(
+        "-l", "--lyrics", type=str, help="set the provided lyrics non-interactively"
+    )
+    parser.add_argument(
+        "-m",
+        "--manual",
+        action="store_true",
+        help="manual interactive mode. Manually set lyrics to one or multiple files",
+    )
     parser.add_argument(
         "-f",
         "--force",
         action="store_true",
-        help="Add or replace lyrics even if lyrics tag is already present",
+        help="add/replace lyrics tag even the tag is already present",
     )
     parser.add_argument(
         "-y",
         "--always_yes",
         action="store_true",
-        help="Save the lyrics without asking the user if they're correct",
+        help="save the lyrics without asking the user first",
     )
 
     args = parser.parse_args()
@@ -42,14 +71,13 @@ def parse_arguments():
     return args
 
 
-def get_search_string(file, path, force=False):
+def get_search_string(file, path, force):
     title = file["title"]
     album = file["album"]
     artist = file["artist"]
 
     if file["lyrics"] and not force:
-        # print(file['lyrics'])
-        return None
+        raise LyricsTagPresent("")
 
     if not title:
         return os.path.splitext(os.path.basename(path))[0]
@@ -57,7 +85,10 @@ def get_search_string(file, path, force=False):
     return f"{artist} {title}"
 
 
-def find_song(search_string: str, genius_client):
+def get_song_genius(search_string: str, genius_client: Genius):
+    """
+    Search song with genius
+    """
     song = genius_client.search_song(search_string)
 
     try:
@@ -66,78 +97,111 @@ def find_song(search_string: str, genius_client):
         return None
 
 
+def tag_manual(f, filepath, args):
+    content = ""
+
+    song_name = get_search_string(f, filepath, args.force)
+    print(f"Path: {filepath}")
+    print(f"Song: {song_name}")
+    print("--- Provdide lyrics (finish with <ctrl-d>):")
+
+    for line in sys.stdin:
+        content += line
+
+    print("--- Saving provided lyrics..")
+
+    content = content.strip()
+    return content
+
+
+def tag_auto(f, filepath, args):
+    content = ""
+
+    g = Genius(args.token, verbose=False)
+    if not args.search_string:
+        search_string = get_search_string(f, filepath, args.force)
+    else:
+        search_string = args.search_string
+
+    if not search_string:
+        print("--- Skipping", filepath)
+        raise NoGeniusSongFound("Cannot get search string", "")
+
+    # Access Genius API
+    print("--- Search string:", search_string)
+    song = get_song_genius(search_string, g)
+    if not song:
+        raise NoGeniusSongFound("", search_string)
+
+    lyrics = "\n".join(song.lyrics.split("\n")[1:])
+
+    # remove Genius artifacts
+    lyrics = lyrics.replace("You might also like", "")
+    lyrics = re.sub(r"\d*Embed$", "", lyrics)
+    lyrics = lyrics.strip()
+
+    print(f'--- Found song "{song.artist} - {song.title}" with lyrics:')
+    print(f'{"\n".join(lyrics.split("\n"))} \n')
+
+    # Interactive mode prompt
+    if not args.always_yes:
+        print("--- Are these lyrics correct? [<ctrl-d>=yes, input=correct lyrics] ")
+
+        for line in sys.stdin:
+            content += line
+            if line == "":
+                break
+
+    if content == "":
+        print("--- Saving found lyrics")
+    else:
+        print("--- Saving provided lyrics")
+        lyrics = content
+
+    return lyrics
+
+
 def main():
+    args = parse_arguments()
     unfound_songs: List = []
 
-    args = parse_arguments()
-
     files = args.files
-    for filepath in files:
-        f = music_tag.load_file(filepath)
-
-        content = ""
-        if args.manual:  # MANUAL
-            song_name = get_search_string(f, filepath, args.force)
+    try:
+        for filepath in files:
+            print("=============================")
             print(filepath)
-            print(song_name)
 
-            for line in sys.stdin:
-                content += line
-                if line == "":
-                    break
+            f = music_tag.load_file(filepath)
 
-            print("--- Saving provided lyrics..")
-            lyrics = content
-
-        else:  # NON MANUAL
-            g = Genius(args.token)
-            if not args.search_string:
-                search_string = get_search_string(f, filepath, args.force)
-            else:
-                search_string = args.search_string
-
-            if not search_string:
-                print("--- Skipping", filepath)
-                continue
-
-            print("Search string:", search_string)
             if args.lyrics:
+                print("--- Saving provided lyrics..")
                 lyrics = args.lyrics
+
             else:
-                song = find_song(search_string, g)
-                if not song:
-                    unfound_songs.append(search_string)
-                    continue
-                lyrics = "\n".join(song.lyrics.split("\n")[1:]).strip()
-                lyrics = lyrics.replace("You might also like", "")
-
-                # remove Genius artifact
-                lyrics = re.sub(r"\d*Embed$", "", lyrics)
-
-                print(
-                    f"----- Found song: {song.artist} - {song.title} \n\t {'\n'.join(lyrics.split("\n"))} \n"
-                )
-                print("--- Are these correct? [<ctrl-d>=yes, input=correct lyrics] ")
-
-                for line in sys.stdin:
-                    content += line
-                    if line == "":
-                        break
-
-                if content == "":
-                    print("--- Saving the found lyrics..")
+                if args.manual:
+                    lyrics = tag_manual(f, filepath, args)
                 else:
-                    print("--- Saving provided lyrics..")
-                    lyrics = content
-                print()
-                print("=========================")
+                    try:
+                        lyrics = tag_auto(f, filepath, args)
+                    except NoGeniusSongFound as e:
+                        unfound_songs.append(e.search_string)
+                        continue
+                    except LyricsTagPresent as e:
+                        print("--- Lyrics tag already preset, skipping.")
+                        continue
+            print()
 
-        f["lyrics"] = lyrics
-        f.save()
+            f["lyrics"] = lyrics
+            f.save()
 
-    print()
-    for i in unfound_songs:
-        print(f"--- Couldn't find song: {i}")
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected. Exiting.")
+
+    finally:
+        if unfound_songs:
+            print("--- Errors ---")
+            for i in unfound_songs:
+                print(f"--- Couldn't find song: {i}")
 
 
 if __name__ == "__main__":
